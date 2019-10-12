a49dac3f7a673fb104fe0d895e61663af0371576
Christoph Muche <christoph@muche.info>
2019-10-12 22:59:21 +0200

    using lombok

--- src/main/java/cmuche/oxp/entities/Coordinate.java
+++ src/main/java/cmuche/oxp/entities/Coordinate.java
@@ -1,37 +1,16 @@
 package cmuche.oxp.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
-import java.util.Objects;
-
+@EqualsAndHashCode
+@AllArgsConstructor
 public class Coordinate
 {
-  public Coordinate(double lat, double lon)
-  {
-    this.lat = lat;
-    this.lon = lon;
-  }
-
   @Getter
   private double lat;
 
   @Getter
   private double lon;
-
-  @Override
-  public boolean equals(Object o)
-  {
-    if (this == o)
-      return true;
-    if (o == null || getClass() != o.getClass())
-      return false;
-    Coordinate that = (Coordinate) o;
-    return Double.compare(that.lat, lat) == 0 && Double.compare(that.lon, lon) == 0;
-  }
-
-  @Override
-  public int hashCode()
-  {
-    return Objects.hash(lat, lon);
-  }
 }
