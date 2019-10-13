769b57b8a26b56d23b048203a041126895265d9e
Christoph Muche <christoph@muche.info>
2019-10-13 21:18:28 +0200

    clear app

--- src/main/java/cmuche/oxp/App.java
+++ src/main/java/cmuche/oxp/App.java
@@ -1,16 +1,9 @@
 package cmuche.oxp;
 
-import cmuche.oxp.entities.Way;
-import cmuche.oxp.parsing.OxpParser;
-
-import java.io.File;
-import java.util.Set;
-
 public class App
 {
-  public static void main(String[] args) throws Exception
+  public static void main(String[] args)
   {
-    Osm osm = OxpParser.parseOsmFile(new File("map.osm"));
-    Set<Way> areas = osm.find().ways().isArea().results();
+
   }
 }
