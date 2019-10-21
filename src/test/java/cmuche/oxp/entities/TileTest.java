779b9ebb11bc1b96c668443ba6aabd180ff871ac
Christoph Muche <christoph@muche.info>
2019-10-21 22:32:02 +0200

    test fix

--- src/test/java/cmuche/oxp/entities/TileTest.java
+++ src/test/java/cmuche/oxp/entities/TileTest.java
@@ -18,7 +18,7 @@ public class TileTest
   {
     Tile tile = new Tile(70406, 42987, 17);
     BoundingBox bbox = tile.getBoundingBox();
-    BoundingBox expected = new BoundingBox(52.517892228, 52.516220864, 13.375854492, 13.378601074);
+    BoundingBox expected = new BoundingBox(52.516220864, 52.517892228, 13.375854492, 13.378601074);
     Assert.assertEquals(expected.getLatMin(), bbox.getLatMin(), 0.00001d);
     Assert.assertEquals(expected.getLatMax(), bbox.getLatMax(), 0.00001d);
     Assert.assertEquals(expected.getLonMin(), bbox.getLonMin(), 0.00001d);
