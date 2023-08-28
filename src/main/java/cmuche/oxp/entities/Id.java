b18a3110055f28a63a3fc08334c4bc7580a7e08f
Christoph Muche <christoph@muche.info>
2023-08-29 01:04:51 +0200

    fix

--- src/main/java/cmuche/oxp/entities/Id.java
+++ src/main/java/cmuche/oxp/entities/Id.java
@@ -3,6 +3,8 @@ package cmuche.oxp.entities;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
 
 @EqualsAndHashCode
 @AllArgsConstructor
@@ -16,6 +18,24 @@ public class Id
 
   @Getter
   private int version;
+
+  @Override
+  public boolean equals(Object o)
+  {
+    if (this == o) return true;
+
+    if (o == null || getClass() != o.getClass()) return false;
+
+    Id id1 = (Id) o;
+
+    return new EqualsBuilder().append(type, id1.type).append(id, id1.id).isEquals();
+  }
+
+  @Override
+  public int hashCode()
+  {
+    return new HashCodeBuilder(17, 37).append(type).append(id).toHashCode();
+  }
 
   @Override
   public String toString()
